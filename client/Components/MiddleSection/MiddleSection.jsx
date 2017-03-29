a9973091e4c4321cd103686a86a47613ca4d6150
bintou karamoko <bintoukaramoko3@gmail.com>
2017-03-29 18:37:49 +0200

    begin middllesection

--- client/Components/MiddleSection/MiddleSection.jsx
+++ client/Components/MiddleSection/MiddleSection.jsx
@@ -1,14 +1,31 @@
+
 import React from 'react';
 import "./MiddleSection.scss";
+import img1 from "../../assets/img1.jpg"
+
 
 
 class MiddleSection extends React.Component {
   render() {
     return (
        <div className="MiddleSection">
+           <div className="container col-lg-12 ">
+              <div className="img-contain">
+                <img src={img1} width="50%" alt=""/>
+                <div className="right">
+                    <div className="text">
+                      <div className="title">
+                        <h1>Tattoo Art</h1>
+                      </div>
+                      <div className="body">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                      </div>
 
-           <div className="container-fluid col-lg-12">
-               <h1 className="title col-lg-4  col-md-4">Hello world</h1>
+                </div>
+                </div>
+
+              </div>
+
            </div>
 
       </div>
--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,7 @@ module.exports = {
       { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
       { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
       { test: /\.scss$/,loaders: ['style-loader', 'css-loader', 'sass-loader']},
+      {test: /\.(jpe?g|png|gif|svg)$/i, loaders: "file-loader?name=/client/assets/[name].[ext]"},
 
     ]
   },
